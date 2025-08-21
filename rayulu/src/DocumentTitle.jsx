f9e0dd3ae462202a26e18ba2c8889ca41bc6b6f5
rayulu <dasaradharam109@gmail.com>
2025-08-21 21:05:23 +0530

    titles updated

--- rayulu/src/DocumentTitle.jsx
+++ rayulu/src/DocumentTitle.jsx
@@ -7,11 +7,17 @@ const pageTitles = {
   '/about': 'Top Cleaning and Maintance Company in Hyderabad - PCS Care',
   '/registration-certificate': 'Registration Certificate - PCS Care',
   '/contact': 'Best Office & Home Cleaning Company in Hyderabad - PCS Care',
-  '/Corporate-services/carpet-cleaning': 'Corporate Carpet Cleaning Services - PCS Care',
+  '/Corporate-services/carpet-cleaning': 'Top Carpet Cleaning Services - Corporate Services - PCS Care',
   '/home-services/carpet-cleaning': 'Home Carpet Cleaning Services - PCS Care',
   '/home-services/marble-polishing': 'Home Marble Polishing Services - PCS Care',
   '/blog': 'Our Blogs - PCS Care',
   '/corporate-services': 'Industrial and Corporate Services - PCS Care',
+  '/deep-cleaning': 'Professional Deep Cleaning Services - PCS Care',
+  '/marble-polishing': 'Commercial and Office Marble Polishing Services - PCS Care',
+  '/sofa-cleaning': 'Professional Sofa Cleaning Services - PCS Care',
+  '/chair-cleaning': 'Best Office Chair Cleaning Services - PCS Care',
+  '/pest-control': 'Best Pest Control Services - PCS Care',
+  '/workstation-cleaning': 'Workstation, Pannel, Window Blinds, Rollers Cleaning Services - PCS Care',
   
 };
 
